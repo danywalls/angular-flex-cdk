044d8f86d65c0986781cafcda696b786166ea66e
Dany Paredes <danywalls4@gmail.com>
2025-04-24 10:21:51 +0200

    Refactor component to display top movies instead of NBA games
    Replaced NBA results with a list of top movies for 2023. Updated the app structure to include a sidebar menu, a container layout, and styled movie cards. Adjusted SCSS for better visuals and added support for responsive design.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,82 +1,73 @@
-import { Component } from '@angular/core';
-import {DatePipe} from '@angular/common';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    DatePipe
-  ],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
 
 
-  nbaGames = [
+
+  topMovies2023 = [
     {
-      title: 'NBA Game 1',
-      date: '2023-10-01',
-      time: '20:00',
-      homeTeam: 'Lakers',
-      awayTeam: 'Warriors',
-      homeScore: 102,
-      awayScore: 98,
+      title: 'Oppenheimer',
+      director: 'Christopher Nolan',
+      releaseDate: '2023-07-21',
+      genre: 'Biography/Drama',
+      rating: 8.5,
+      boxOffice: '$950 million'
     },
     {
-      title: 'NBA Game 2',
-      date: '2023-10-02',
-      time: '21:00',
-      homeTeam: 'Celtics',
-      awayTeam: 'Nets',
-      homeScore: 110,
-      awayScore: 105,
+      title: 'Barbie',
+      director: 'Greta Gerwig',
+      releaseDate: '2023-07-21',
+      genre: 'Comedy/Fantasy',
+      rating: 7.3,
+      boxOffice: '$1.4 billion'
     },
     {
-      title: 'NBA Game 3',
-      date: '2023-10-03',
-      time: '22:00',
-      homeTeam: 'Bulls',
-      awayTeam: 'Heat',
-      homeScore: 95,
-      awayScore: 100,
+      title: 'Guardians of the Galaxy Vol. 3',
+      director: 'James Gunn',
+      releaseDate: '2023-05-05',
+      genre: 'Action/Adventure',
+      rating: 8.0,
+      boxOffice: '$845 million'
     },
     {
-      title: 'NBA Game 4',
-      date: '2023-10-04',
-      time: '23:00',
-      homeTeam: 'Suns',
-      awayTeam: 'Mavericks',
-      homeScore: 120,
-      awayScore: 115,
+      title: 'Spider-Man: Across the Spider-Verse',
+      director: 'Joaquim Dos Santos, Kemp Powers, Justin K. Thompson',
+      releaseDate: '2023-06-02',
+      genre: 'Animation/Action',
+      rating: 8.7,
+      boxOffice: '$690 million'
     },
     {
-      title: 'NBA Game 5',
-      date: '2023-10-05',
-      time: '20:30',
-      homeTeam: 'Bucks',
-      awayTeam: '76ers',
-      homeScore: 108,
-      awayScore: 110,
+      title: 'The Super Mario Bros. Movie',
+      director: 'Aaron Horvath, Michael Jelenic',
+      releaseDate: '2023-04-05',
+      genre: 'Animation/Adventure',
+      rating: 7.1,
+      boxOffice: '$1.36 billion'
     },
     {
-      title: 'NBA Game 6',
-      date: '2023-10-06',
-      time: '21:30',
-      homeTeam: 'Clippers',
-      awayTeam: 'Nuggets',
-      homeScore: 115,
-      awayScore: 112,
+      title: 'John Wick: Chapter 4',
+      director: 'Chad Stahelski',
+      releaseDate: '2023-03-24',
+      genre: 'Action/Thriller',
+      rating: 7.8,
+      boxOffice: '$440 million'
     },
     {
-      title: 'NBA Game 7',
-      date: '2023-10-07',
-      time: '22:30',
-      homeTeam: 'Rockets',
-      awayTeam: 'Grizzlies',
-      homeScore: 98,
-      awayScore: 100,
+      title: 'Killers of the Flower Moon',
+      director: 'Martin Scorsese',
+      releaseDate: '2023-10-20',
+      genre: 'Crime/Drama',
+      rating: 8.0,
+      boxOffice: '$156 million'
     }
-
-
   ]
+  menuOptions: string[] = ['Home', 'About', 'Contact'];
+
 }
